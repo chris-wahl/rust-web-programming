bc96ff965ba6098f8128fe6efdb4aeceea4ebfd7
christopher <christopher@nothingmuch.net>
2021-11-15 15:27:30 -0500

    Chapter 2 - add state

--- chapter2/chapter2-todo/src/main.rs
+++ chapter2/chapter2-todo/src/main.rs
@@ -1,12 +1,27 @@
-use to_do::{ItemTypes, to_do_factory, structs::traits::create::Create};
+use std::env;
 
+use serde_json::{json, Map};
+use serde_json::value::Value;
+
+use state::{read_file, write_to_file};
+use to_do::{ItemTypes, structs::traits::create::Create, to_do_factory};
+
+mod state;
 mod to_do;
 
+const FILEPATH: &str = "/home/christopher/Documents/Rust/Rust Web Programming/chapter2/chapter2-todo/src/state.json";
 
 fn main() {
-    let todo_item = to_do_factory("pending", "washing");
-    match todo_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
-        ItemTypes::Done(item) => println!("Pending item: {}", item.super_struct.title),
+    let args: Vec<String> = env::args().collect();
+
+    let mut state = read_file(FILEPATH);
+    println!["{:?}", state];
+
+    if args.len() > 2 {
+        let status = &args[1];
+        let title = &args[2];
+
+        state.insert(title.to_string(), json!(status));
+        write_to_file(FILEPATH, &mut state);
     }
 }
