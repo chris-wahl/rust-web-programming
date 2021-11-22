0d2e2e0da06894c1c2e0ab1cdf864ca9dc220f7d
christopher <christopher@nothingmuch.net>
2021-11-22 14:53:49 -0500

    Chapter 7 - init chapter

---
+++ section3/chapter7/user_sessions/src/main.rs
@@ -0,0 +1,45 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use actix_web::{App, dev::Service, HttpServer};
+
+mod state;
+mod to_do;
+mod json_serialization;
+mod views;
+mod processes;
+mod database;
+mod schema;
+mod models;
+
+const ADDR: &str = "127.0.0.1:8000";
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                if *&req.path().contains("/item/") {
+                    match views::token::process_token(&req) {
+                        Ok(_token) => println!(
+                            "the token is passable"),
+                        Err(message) => println!(
+                            "token error: {}", message)
+                    }
+                }
+                let fut = srv.call(req);
+                async {
+                    let result = fut.await?;
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory);
+
+        println!["http://{}", ADDR];
+        return app;
+    })
+        .bind(ADDR)?
+        .run()
+        .await
+}
