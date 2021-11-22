2aaf5439179cf59ef5bd5e8a47429fcbee7f34f2
christopher <christopher@nothingmuch.net>
2021-11-22 11:25:49 -0500

    Chapter 5 - end of chapter

--- section2/chapter5/displaying_content/src/views/app/items.rs
+++ section2/chapter5/displaying_content/src/views/app/items.rs
@@ -1,6 +1,6 @@
 use actix_web::HttpResponse;
 
-use super::content_loader::read_file;
+use super::content_loader::{add_component, read_file};
 
 pub async fn items() -> HttpResponse {
     let mut html_data = read_file("./templates/main.html");
@@ -11,6 +11,7 @@ pub async fn items() -> HttpResponse {
     html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data)
         .replace("{{CSS}}", &css_data)
         .replace("{{BASE_CSS}}", &base_css_data);
+    html_data = add_component(String::from("header"), html_data);
 
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
