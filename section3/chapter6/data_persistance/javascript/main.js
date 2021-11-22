ddf85906efceeacc89991f88949cca795456e9eb
christopher <christopher@nothingmuch.net>
2021-11-22 11:50:36 -0500

    Init Section 3

---
+++ section3/chapter6/data_persistance/javascript/main.js
@@ -0,0 +1,71 @@
+function getItems() {
+    let call = apiCall("/item/get", "GET");
+    call.send();
+}
+
+function renderItems(items, processType, elementId, processFunction) {
+    let placeholder = "<div>";
+    const itemsMeta = [];
+    for (let i = 0; i < items.length; i++) {
+        const title = items[i]["title"];
+        const placeholderId = `${processType}-${title.replaceAll(" ", "-")}`;
+        placeholder += '<div class="itemContainer">' + `<p>${title}</p>` +
+            `<div id="${placeholderId}" class="actionButton">${processType}</div>` +
+            "</div>";
+        itemsMeta.push({id: placeholderId, title});
+    }
+    placeholder += "</div>";
+    document.getElementById(elementId).innerHTML = placeholder;
+
+    for (let i = 0; i < itemsMeta.length; i++) {
+        document.getElementById(
+            itemsMeta[i]["id"]
+        ).addEventListener("click", processFunction);
+    }
+}
+
+function createItem() {
+    const title = document.getElementById("name");
+    const call = apiCall("/item/create/" + title.value, "POST");
+    call.send();
+    document.getElementById("name").value = null;
+}
+
+function editItem() {
+    const title = this.id.replaceAll("-", " ").replace("edit ", "");
+    const json = {title, status: "done"};
+
+    const call = apiCall("/item/edit", "PUT");
+    call.send(JSON.stringify(json));
+}
+
+function deleteItem() {
+    const title = this.id.replaceAll("-", " ").replace("delete ", "");
+    const json = {title, status: "done"};
+    const call = apiCall("/item/delete", "DELETE");
+    call.send(JSON.stringify(json));
+}
+
+function apiCall(url, method) {
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            renderItems(JSON.parse(this.responseText)["pending_items"], "edit", "pendingItems", editItem);
+            renderItems(JSON.parse(this.responseText)["done_items"], "delete", "doneItems", deleteItem);
+            document.getElementById("completeNum").innerHTML =
+                JSON.parse(this.responseText)["done_item_count"];
+            document.getElementById("pendingNum").innerHTML = JSON.parse(
+                this.responseText)["pending_item_count"];
+        }
+    });
+    xhr.open(method, url);
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("user-token", "token");
+    return xhr;
+}
+
+document.getElementById("create-button").addEventListener("click", createItem);
+
+getItems();
---
+++ section3/chapter6/data_persistance/src/main.rs
@@ -0,0 +1,37 @@
+use actix_web::{App, dev::Service, HttpServer};
+mod state;
+mod to_do;
+mod json_serialization;
+mod views;
+mod processes;
+
+const ADDR: &str = "127.0.0.1:8000";
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                if *&req.path().contains("/item/") {
+                    match views::token::process_token(&req) {
+                        Ok(_token) => println!(
+                            "the token is passable"),
+                        Err(message) => println!(
+                            "token error: {}", message)
+                    }
+                }
+                let fut = srv.call(req);
+                async {
+                    let result = fut.await?;
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory);
+
+        println!["http://{}", ADDR];
+        return app
+    })
+        .bind(ADDR)?
+        .run()
+        .await
+}
