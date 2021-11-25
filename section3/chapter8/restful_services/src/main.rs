43da23523b83d81e3ce48b6e26fe1e90ebf67fa9
Christopher Wahl <christopher@nothingmuch.net>
2021-11-25 12:01:29 -0500

    Chapter 8 - added comments

--- section3/chapter8/restful_services/src/main.rs
+++ section3/chapter8/restful_services/src/main.rs
@@ -32,14 +32,19 @@ async fn main() -> std::io::Result<()> {
                 } else {
                     true
                 };
-
+                // Using the futures::Either enum instead of separate
+                // `async {}` blocks, as each `async {}` block is treated like a closure; they
+                // each have their own types.  Rust will dislike if one block is returned on `passed` true,
+                // and a different one on `passed` false, as they'd effectively be different types.
+                //
+                //  The Either enum wraps the futures into effectively the same type.
                 let end_result = match passed {
                     // token passed, or is not required
                     true => Either::Left(srv.call(req)),
                     // token failed
                     false => {
                         Either::Right(
-                            ok(
+                            ok( // ok() type makes a future that is immediately ready to go
                                 req.into_response(
                                     HttpResponse::Unauthorized().finish().into_body()
                                 )
